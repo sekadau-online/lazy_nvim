0d0e664780535b2a021eb91abfb87be45d9bbdf9
sekadau-online <74548531+sekadau-online@users.noreply.github.com>
2025-10-23 07:50:49 +0700

    Create keymaps.lua

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- File explorer
+map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Telescope finders
+map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+map("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+map("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
+-- LSP
+map("n", "gd", vim.lsp.buf.definition, opts)
+map("n", "K", vim.lsp.buf.hover, opts)
+map("n", "<leader>rn", vim.lsp.buf.rename, opts)
