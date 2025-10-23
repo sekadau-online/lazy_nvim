9e5d3519f86904e14210ff9d39886d3cff0277a8
sekadau-online <74548531+sekadau-online@users.noreply.github.com>
2025-10-23 08:18:14 +0700

    Update tools.lua

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,13 +1,18 @@
 return {
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  -- Dependensi umum
+  { "nvim-lua/plenary.nvim" },
 
-  -- Git integration
-  { "lewis6991/gitsigns.nvim", config = function()
-      require("gitsigns").setup()
-    end },
-
-  -- Floating terminal
-  { "akinsho/toggleterm.nvim", config = function()
-      require("toggleterm").setup({ size = 15, open_mapping = [[<c-\>]] })
-    end },
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          layout_strategy = "horizontal",
+          layout_config = { width = 0.9, height = 0.8 },
+        },
+      })
+    end,
+  },
 }
