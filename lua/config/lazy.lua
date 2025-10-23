8dba475dd98f0eb88c705308655afbac94b03c35
sekadau-online <74548531+sekadau-online@users.noreply.github.com>
2025-10-23 08:14:49 +0700

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,16 +1,6 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
+-- =========================
+-- Lazy.nvim configuration
+-- =========================
 require("lazy").setup({
   spec = {
     { import = "plugins.ui" },
@@ -19,5 +9,5 @@ require("lazy").setup({
     { import = "plugins.tools" },
   },
   install = { colorscheme = { "tokyonight" } },
-  checker = { enabled = true }, -- auto update
+  checker = { enabled = true }, -- auto update plugin
 })
