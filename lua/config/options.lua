ec11ac43a036e8a240ef1831db8c348350d82daa
sekadau-online <74548531+sekadau-online@users.noreply.github.com>
2025-10-23 07:50:22 +0700

    Create options.lua

---
+++ lua/config/options.lua
@@ -0,0 +1,14 @@
+-- General settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 8
