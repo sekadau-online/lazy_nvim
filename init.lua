6063efecb8c3d802c678e334f46c97489b62fa0a
sekadau-online <74548531+sekadau-online@users.noreply.github.com>
2025-10-23 08:14:18 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,26 @@
--- ====================================
--- Neovim Modular Config (Lazy.nvim)
--- ====================================
+-- =========================
+-- Bootstrap lazy.nvim
+-- =========================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
+-- =========================
+-- Load core configs
+-- =========================
 require("config.options")
 require("config.keymaps")
+
+-- =========================
+-- Load Lazy.nvim plugin setup
+-- =========================
 require("config.lazy")
