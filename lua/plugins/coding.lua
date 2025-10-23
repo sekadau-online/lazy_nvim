dd8070dc6e97641ac303a4e329c4b1e36df4ec1b
sekadau-online <74548531+sekadau-online@users.noreply.github.com>
2025-10-23 08:18:00 +0700

    Update coding.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,11 +1,45 @@
 return {
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = { "lua", "python", "javascript", "go", "html", "css" },
+      })
+    end,
+  },
 
-  -- Auto pairs
-  { "windwp/nvim-autopairs", config = function()
-      require("nvim-autopairs").setup()
-    end },
-
-  -- Comment toggler
-  { "numToStr/Comment.nvim", config = true },
+  -- Autocomplete
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
 }
